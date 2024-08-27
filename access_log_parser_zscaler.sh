a686a331c135a110ed00e3b6b23bc5e4a3c5340a
lei <oizhaolei@gmail.com>
2024-08-27 16:29:49 +0900

    improve the regex

--- access_log_parser_zscaler.sh
+++ access_log_parser_zscaler.sh
@@ -30,6 +30,6 @@ if [ -z "$input" ]; then
 fi
 
 # Read from input file, parse with regex, and write to the csv file
-regex="(?<syslog_time>.{15}) (?<cip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}) \"(?<proxy_ip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\",\"(?<login_id>[^\"]+)\",\"(?<local_time>.{24})\",\"(?<req_method>GET|HEAD|POST|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH)\",\"(?<remote_host>[^\"]+)\",\"(?<s_ip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\",\"(?<resp_code>\d{3})\",\"(?<req_size>\d+)\",\"(?<resp_size>\d+)\",\"(?<url_super_cat>[^\"]+)\",\"(?<url_cat>[^\"]+)\",\"(?<rist_score>\d+)\",\"(?<ua>[^\"]+)\",\"(?<action>[^\"]+)\",\"(?<rule_label>[^\"]+)\",\"(?<url_filter_rule_label>[^\"]+)\",\"(?<reason>[^\"]+)\",\"(?<ssl_decrypted>[^\"]+)\",\"(?<external_spr>[^\"]+)\".*"
+regex="(?<syslog_time>.{15}) (?<cip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}) \"(?<proxy_ip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\",\"(?<login_id>[^\"]+)\",\"(?<local_time>.{24})\",\"(?<req_method>GET|HEAD|POST|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH|NA)\",\"(?<remote_host>.+)\",\"(?<s_ip>[^\"]+)\",\"(?<resp_code>[^\"]+)\",\"(?<req_size>\d+)\",\"(?<resp_size>\d+)\",\"(?<url_super_cat>[^\"]+)\",\"(?<url_cat>[^\"]+)\",\"(?<rist_score>\d+)\",\"(?<ua>[^\"]+)\",\"(?<action>[^\"]+)\",\"(?<rule_label>[^\"]+)\",\"(?<url_filter_rule_label>[^\"]+)\",\"(?<reason>[^\"]+)\",\"(?<ssl_decrypted>[^\"]+)\",\"(?<external_spr>[^\"]+)\".*"
 
 target/debug/access_log_parser --regex "$regex" --input "$input"
